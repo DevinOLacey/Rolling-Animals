acf9e938c829a67b23d8f45ef7676817536727f6
Quizgenm <144553643+Quizgenm@users.noreply.github.com>
2026-04-18 21:16:37 -0700

    Implement Die class with roll method

--- dice.cpp
+++ dice.cpp
@@ -1,1 +1,13 @@
+#include "Dice.h"
+#include <cstdlib> // for rand()
 
+Die::Die(int s) {
+    if (s < 2) {
+        throw DiceException("Die must have at least 2 sides.");
+    }
+    sides = s;
+}
+
+int Die::roll() const {
+    return (rand() % sides) + 1;
+}
