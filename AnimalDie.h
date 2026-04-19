c730d78be2dacd0add94b3e2fb324927f3e0f968
Quizgenm <144553643+Quizgenm@users.noreply.github.com>
2026-04-19 00:17:10 -0700

    Create AnimalDie.h

---
+++ AnimalDie.h
@@ -0,0 +1,27 @@
+#ifndef ANIMAL_DIE_H
+#define ANIMAL_DIE_H
+
+#include "Dice.h"
+#include <string>
+
+class AnimalDie : public Die {
+
+protected:
+    std::string animalName;   // name/type of animal (Lion, Tiger, etc.)
+    int animalValues[6];      // Values for each side of the die
+
+public:
+    AnimalDie();
+    AnimalDie(const std::string& type, const int values[6]);
+
+    int roll() const override;
+
+    std::string getAnimalType() const;
+
+    void printInfo() const override;
+
+    bool loadFromFile(const std::string& filename);
+    bool saveToFile(const std::string& filename) const;
+};
+
+#endif
