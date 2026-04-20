96d2783eb0adef85825af57fd162388280063a31
Quizgenm <144553643+Quizgenm@users.noreply.github.com>
2026-04-20 01:30:42 -0700

    Refactor TransformDice classes to use MagicDice

--- TransDie.h
+++ TransDie.h
@@ -4,53 +4,38 @@
 #include "MagicDice.h"
 #include <cstdlib>
 
-// Base for dice that transform roll mathematically
-class TransformDice : public MagicDice {
-protected:
-    int sides;
-
-public:
-    TransformDice(int s = 6) : sides(s) {}
-
-    std::string getType() const override {
-        return "Transform";
-    }
-};
-
-// FLIP
-class FlipDice : public TransformDice {
+// FLIP, filps the player's roll to the opposite side of the die (1 becomes 6, 2 becomes 5, etc.)
+class FlipDice : public MagicDice {
 public:
     int applyEffect(int playerRoll, int) const override {
-        return (sides + 1) - playerRoll;
+        return 7 - playerRoll;
     }
 
-    std::string getType() const override {
-        return "Flip";
-    }
+    std::string getType() const override { return "Flip"; }
+    std::string getCategory() const override { return "Transform"; }
 };
 
-// REROLL
-class RerollDice : public TransformDice {
+// REROLL, rerolls the player's die and takes the new result instead of the original roll.
+class RerollDice : public MagicDice {
 public:
     int applyEffect(int, int) const override {
-        return (rand() % sides) + 1;
+        return (rand() % 6) + 1;
     }
 
-    std::string getType() const override {
-        return "Reroll";
-    }
+    std::string getType() const override { return "Reroll"; }
+    std::string getCategory() const override { return "Transform"; }
 };
 
-// PILLAR
-class PillarDice : public TransformDice {
+// Pillar, adds the bottom of the "die" to the player's roll, EX: if the player rolls a 2 , they get a boost of 5 (7 - 2) for a total of 7. If they roll a 5, they get a boost of 2 (7 - 5) for a total of 7. 
+// This is the most powerful transform die, since it can guarantee a decently high roll, but it is also the hardest to activate since it can be very powerful.
+class PillarDice : public MagicDice {
 public:
     int applyEffect(int playerRoll, int) const override {
-        return playerRoll + (playerRoll - 1);
+        return 7 - playerRoll;
     }
 
-    std::string getType() const override {
-        return "Pillar";
-    }
+    std::string getType() const override { return "Pillar"; }
+    std::string getCategory() const override { return "Transform"; }
 };
 
 #endif
